e27dc71da371c1fb7197243bd193bb7674a9d854
MH2014 <1221561@uni-wuppertal.de>
2014-12-26 10:57:26 +0100

    NotificationService2: verbesserter Listener

--- Testphase/AppTemplate/gen/com/test/R.java
+++ Testphase/AppTemplate/gen/com/test/R.java
@@ -14,11 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int einstellungen=0x7f080003;
-        public static final int exit=0x7f080004;
-        public static final int texti=0x7f080001;
-        public static final int time=0x7f080002;
-        public static final int title=0x7f080000;
+        public static final int button1=0x7f080000;
+        public static final int einstellungen=0x7f080005;
+        public static final int exit=0x7f080006;
+        public static final int info=0x7f080004;
+        public static final int texti=0x7f080002;
+        public static final int time=0x7f080003;
+        public static final int title=0x7f080001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -29,7 +31,9 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050001;
         public static final int buttontest=0x7f050002;
+        public static final int buttontest2=0x7f050008;
         public static final int hello=0x7f050000;
+        public static final int info=0x7f050009;
         public static final int label1=0x7f050003;
         public static final int service_description=0x7f050004;
         public static final int texti=0x7f050006;
