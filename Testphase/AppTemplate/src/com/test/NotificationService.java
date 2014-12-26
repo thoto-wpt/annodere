bae5307f9ebe5c64d6c3d99be2c10fad2eb278c6
MH2014 <1221561@uni-wuppertal.de>
2014-12-26 10:39:16 +0100

    NotificationService2: verbesserter Listener

--- Testphase/AppTemplate/src/com/test/NotificationService.java
+++ Testphase/AppTemplate/src/com/test/NotificationService.java
@@ -1,10 +1,15 @@
 package com.test;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.app.Notification;
-import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
 //import android.database.Cursor;
@@ -24,6 +29,7 @@ public class NotificationService extends AccessibilityService
 	      {
 			Notification notification = (Notification) event.getParcelableData();
 
+			  /*// Unwichtige, aber interessante Details:
 			  Log.d("INC", "Incoming notification!");
 			  Log.d("PACK", "App: " + event.getPackageName());
 			  Log.d("TEXT", "Ticker: " + notification.tickerText);
@@ -31,6 +37,71 @@ public class NotificationService extends AccessibilityService
 			  Log.d("TIME", "Uhr: " + event.getEventTime());
 			  Log.d("TYP", "Typ: " + event.getEventType());
 			  Log.d("ITXT", "Text: " + event.getText());
+			  */
+			
+			  //List<CharSequence> notificationList = event.getText();
+			  //System.out.println(notificationList); // Möglichkeiten über Möglichkeiten ...
+			 
+			
+			  System.out.println(notification); // So sieht die normale, nicht geparste Version aus.
+			  
+			  // Parser: 
+			  // http://stackoverflow.com/questions/9292032/extract-notification-text-from-parcelable-contentview-or-contentintent
+			  // unterster Beitrag
+			  
+			  RemoteViews views = notification.contentView;
+			  Class secretClass = views.getClass();
+
+			  try {
+			      Map<Integer, String> text = new HashMap<Integer, String>();
+
+			      Field outerField = secretClass.getDeclaredField("mActions");
+			      outerField.setAccessible(true);
+			      ArrayList<Object> actions = (ArrayList<Object>) outerField.get(views);
+
+			      for (Object action : actions) {
+			          Field innerFields[] = action.getClass().getDeclaredFields();
+			          Field innerFieldsSuper[] = action.getClass().getSuperclass().getDeclaredFields();
+
+			          Object value = null;
+			          Integer type = null;
+			          Integer viewId = null;
+			          for (Field field : innerFields) {
+			              field.setAccessible(true);
+			              if (field.getName().equals("value")) {
+			                  value = field.get(action);
+			              } else if (field.getName().equals("type")) {
+			                  type = field.getInt(action);
+			              }
+			          }
+			          for (Field field : innerFieldsSuper) {
+			              field.setAccessible(true);
+			              if (field.getName().equals("viewId")) {
+			                  viewId = field.getInt(action);
+			              }
+			          }
+
+			          if (value != null && type != null && viewId != null && (type == 9 || type == 10)) {
+			              text.put(viewId, value.toString());
+			          }
+			      }
+
+			      System.out.println("title is: " + text.get(16908310));
+			      System.out.println("info is: " + text.get(16909082));
+			      System.out.println("text is: " + text.get(16908358));
+			      
+			      MainActivity.receiveNoti(text);
+			      
+			  } 
+			  
+			  catch (Exception e) {
+			      e.printStackTrace();
+			  }
+			  
+			  
+			  
+			  
+			  
 			  
 			  // Das oben zeigt an, was ich bisher auslesen kann
 			  // Die Daten sind aber noch verschlüsselt in den Paketen => Pakete müssen geparst werden
@@ -46,6 +117,7 @@ public class NotificationService extends AccessibilityService
 			  c.close();
 			  
 			  Log.d("SMS", "Text: " + sms.getBody() + sms.getNumber());*/
+			  
 	      }
 	}
 
