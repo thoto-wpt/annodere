9839511a27a0d828c2ae0f93a8b85330a9ab7620
Thorsten M <thoto@uller.thoto.tk>
2015-01-07 01:30:41 +0100

    POST implemented, Rpc_method started
    * POST requests to RPC-Server are ready to use. Debugging output appers on
    	standard output.
    * RPC method call interface started, but not quite ready to use.
    * It may be possible that the RPC method call interface will be rewritten.
    * Various minor changes

--- nettest/rpc_server.cpp
+++ nettest/rpc_server.cpp
@@ -1,63 +1,192 @@
 #include"rpc_server.h"
+#include<cassert>
+#include<cstring>
 
 namespace Annodere{
 	using namespace std;
 
-	bool Rpc_call::compatible(Rpc_call& def) {
-		if(num_params!=def.num_params) return false;
+	/**
+	 * Method call to register client to server
+	 * @returns token
+	 **/
+	Json::Value Rpc_method_register::call(Json::Value){
+		return nullptr;
+	}
+
+	/**
+	 * Check compatibility between actual call and method signature
+	 * @returns true if you may run this method call
+	 **/
+	bool Rpc_method::compatible(Json::Value& params) {
+		if(params==nullptr && arguments.size()==0) return true;
+		else if(!params.isArray())
+			return arguments.size()==1&&params.isConvertibleTo(arguments[0]);
+		if(params.size()!=arguments.size()) return false;
+		
+		int i=0;
+		for(auto p: params){
+			if(!p.isConvertibleTo(arguments[i])) return false;
+		}
+
+/*		if(num_params!=def.num_params) return false;
 		auto dp=(def.params)->begin();
 		for(auto p: *params){
 //		for(int i=0;i<num_params;i++){
 //			if(!params[i].isConvertibleTo(def.params[i].type())) return false;
 			if(p.isConvertibleTo((*dp).type())) return false;
 			dp++;
-		}
-		return true;
+		}*/
+		return true; 
 	}
 
-/* 	static int* Rpc_call::spdi(void* cls, MHD_ValueKind kind, const char* key,
-			const char* filename, const char* content_type,
-			const char* transfer_encoding, const char* data, uint64_t off,
-			size_t size){// post data iterator
-		Rpc_call* t=static_cast<Rpc_call*>(cls);
-		return t->pdi(kind, key, filename, content_type, transfer_encoding,
-			data, off, size);
-	} */
-		
-
-	Rpc_call Rpc_server::get_method(Json::Value &json){
-		Rpc_call call;
-
-		Json::Value version=json["jsonrpc"];
-		Json::Value id=json["id"];
-		Json::Value method=json["method"];
-		Json::Value params=json["params"];
-
-		if(!(version.isString()&&version.asString()=="2.0")){
-			call.err=err_request; return call;
-		}
-
-		if(method.isString()) call.method=method.asString();
-		else{ call.err=err_request; return call; }
-
-		if(id.isString()||id.isUInt()) call.id=id.asString();
-		else if(id.isNull()) call.id_is_null=true;
-		else{ call.err=err_request; return call; }
-
-		if(params.isArray()) call.params=new Json::Value(params);
-		else if(params.isNull()) call.params=nullptr;
-		else{ call.err=err_request; return call; }
-
-		return call;
+	/**
+	 * get JSON RPC call for debugging reasons
+	 * @returns RPC request in JSON fromat
+	 **/
+	string Rpc_call::get_json_rpc(void){
+		return callstr;
 	}
 
+	/**
+	 * decodes POST-encoded data
+	 * @params post	the data to be decoded
+	 **/
+	void Rpc_call::unescape_post(string& post){
+		size_t i,j; // FIXME: Type?
+		string res;
+		i=0x00;j=-0x01; // start and end of chunk
+		res.reserve(post.length()); // urldecoded can't be longer
+		while((j=post.find_first_of("+%",j+1))!=string::npos){
+			if(post[j]=='+') post[j]=' ';// replace + by space
+			else if(post[j]=='%') { // decode urlencoded
+				post[j]=stoul(post.substr(j+1,2),nullptr,16); // hex!
+				res.append(post.substr(i,j-i+1));
+				i=j+3;
+			}
+		} res.append(post.substr(i,string::npos)); // fill up with rest
+		post=res;
+	}
+	
+	/**
+	 * processes chunk of post data sent by MHD
+	 * @param creq		request data
+	 * @param length	length of data chunk
+	 * @return sequence of data chunk
+	 **/
+	int Rpc_call::process(const char* creq,size_t length){
+		post_data.emplace(creq,length);
+		post_size+=length;
+		return call_num++;
+	}
+
+	/**
+	 * parses POST request and parses JSON if request data found
+	 * @warning may consume large amounts of memory if large request gets in
+	 * @returns error code or 0x00 on success
+	 **/
+	signed int Rpc_call::parse(){
+		// TODO: prevent DOS by limiting request size
+		string post_string;
+
+		// ===
+		// put post request data together
+		post_string.reserve(post_size);
+		while(!post_data.empty()){
+			post_string+=post_data.front();
+			post_data.pop();
+		}
+
+		// ===
+		// parse post data
+		size_t from, to, end;// FIXME Type? (64 bits)
+		from=0x00; // start of var name
+		to=post_string.find("="); // end of var name
+		while(to!=string::npos){
+			assert(from!=string::npos);
+			end=post_string.find("&",to); // find seperator to next variable
+
+			// we just look for request variable and store it
+			if(post_string.substr(from,to-from)=="request")
+				callstr=post_string.substr(to+1,
+					(end==string::npos)?end:end-to-1);
+
+			// set start of variable name to end of current variable content
+			from=(end==string::npos)?end:end+1;
+			to=post_string.find("=",from); // look for start of variable content
+		}
+		if(callstr.empty()) // unable to get request data
+			return Rpc_server::err_request;
+
+		// ===
+		// decode request
+		unescape_post(callstr);
+
+		// ===
+		// finally parse JSON data
+		Json::Reader json_reader;
+		if(!json_reader.parse(callstr, json_root, false))
+			return Rpc_server::err_parse;
+
+		if(json_root.isArray()) {
+			return Rpc_server::err_internal; // TODO
+		}else return json_parse();
+	}
+
+	signed int Rpc_call::json_parse(){
+		Json::Value jv_version=json_root["jsonrpc"];
+		jv_id=json_root["id"]; // declared at object
+		Json::Value jv_method=json_root["method"];
+		Json::Value jv_params=json_root["params"];
+
+		// version string must match
+		if(!(jv_version.isString()&&jv_version.asString()=="2.0"))
+			return Rpc_server::err_request;
+
+		// method must be string
+		if(!jv_method.isString()) return Rpc_server::err_request;
+		else method=jv_method.asString();
+
+		// id must be present as null value or as unsigned integer or string
+		if(jv_id.isString()||jv_id.isUInt()) id=jv_id.asString();
+		else if(jv_id.isNull()) id_is_null=true;
+		else return Rpc_server::err_request;
+
+		// there may be parameters
+		if(jv_params.isArray()) params=new Json::Value(jv_params);
+		else if(jv_params.isNull()) params=nullptr;
+		return 0x00;
+	}
+
+	/**
+	 * Rpc_call constructor
+	 **/
+	Rpc_call::Rpc_call() : id_is_null(false),post_size(0), call_num(0){
+		params=nullptr;
+	}
+
+	/**
+	 * Rpc_call constructor for multi method calls
+	 **/
+	Rpc_call::Rpc_call(Json::Value val) : id_is_null(false),post_size(0),
+			call_num(0){
+		json_root=val;
+		params=nullptr;
+		json_parse();
+	}
+
+	/**
+	 * TODO
+	 **/
 	void* Rpc_server::get_key(void* cls, enum MHD_ValueKind kind,
 			const char* c_key, const char* c_value){
 		//...
 		UNUSED(cls); UNUSED(kind); UNUSED(c_key); UNUSED(c_value);
 		return nullptr;
 	}
-		
+
+	/**
+	 * Handles every HTTP request to MHD server instance
+	 **/
 	int Rpc_server::handler(struct MHD_Connection* connection,
 			const char* c_url, const char* method,
 			const char* version, const char* upload_data,
@@ -68,14 +197,42 @@ namespace Annodere{
 		struct MHD_Response* response;
 		int ret;
 		string data; int code;
+
+
+		string url=c_url;
+		if(url=="/annodere"){
+			if(*con_cls==NULL){ // new call: create Rpc_call
+				printf("URL: %s, %d\n",url.c_str(),port);
+				Rpc_call* call=new Rpc_call();
+				*con_cls=static_cast<void*>(call);
+				return MHD_YES;
+			}else{
+				// process post data
+				Rpc_call* call=static_cast<Rpc_call*>(*con_cls);
+				 if(*upload_data_size){
+					call->process(upload_data,*upload_data_size);
+					*upload_data_size=0x00;
+					return MHD_YES;
+				}else{ // recieving post data finished: process request.
+					signed int err;
+					if((err=call->parse())==0x00){
+						data="{state:true}";
+						code=200;
+					}else{
+						data=generate_error(err);
+						code=422;
+					}
+					printf("\t%s\n",(call->get_json_rpc()).c_str());
+					delete call;
+				}
+			}
+
+
+/*
 		Json::Reader json_reader;
 		Json::Value json_root;
 		string request;
-		
 
-		string url=c_url;
-		printf("URL: %s, %d\n",url.c_str(),port);
-		if(url=="/annodere"){
 			if(upload_data!=NULL) // C!
 				request=string(upload_data, *upload_data_size);
 			else request="EMPTY"; 
@@ -106,7 +263,7 @@ namespace Annodere{
 					data="{state:true}";
 					code=200;
 				}
-			}
+			}*/
 		}else{
 			printf("\t404\n");
 			data=generate_error(err_parse);
--- nettest/rpc_server.h
+++ nettest/rpc_server.h
@@ -9,6 +9,7 @@ extern "C"{
 }
 #endif
 #include<string>
+#include<queue>
 #include<jsoncpp/json/json.h>
 
 #define UNUSED(expr) (void)(expr)
@@ -16,23 +17,51 @@ extern "C"{
 namespace Annodere{
 	using namespace std;
 
-	// enum class Rpc_type{...};
+	class Rpc_method{
+		public:
+			const string name;
+			const vector<Json::ValueType> arguments;
+			virtual Json::Value call(Json::Value)=0;
+			bool compatible(Json::Value& params);
+	};
+
+	class Rpc_method_register: public Rpc_method{
+		public:
+			const string name="register";
+			const vector<Json::ValueType> arguments=
+				{Json::intValue,Json::stringValue};
+			Json::Value call(Json::Value);
+	};
+
 	class Rpc_call{
 		public:
-			Rpc_call() : err(0),num_params(0),id_is_null(false){};
-			int err;
+			Rpc_call();
+			Rpc_call(Json::Value);
+			~Rpc_call(){ if(params!=nullptr) delete params; }
+
+
 			string method;
-			int num_params;
 			Json::Value* params;
 			bool id_is_null;
 			string id;
-			bool compatible(Rpc_call& def);
-			int* pdi(enum MHD_ValueKind kind, const char* key,
-				const char* filename, const char* content_type,
-				const char* transfer_encoding, const char* data, uint64_t off,
-				size_t size);
+			Json::Value jv_id;
+
+			int process(const char*,size_t);
+			signed int parse();
+			string get_json_rpc();
 		private:
-			string qstring;
+			//! storage for post data chunks
+			queue<string> post_data;
+			//! JSON-RPC call as string
+			string callstr;
+			//! number of chunks FIXME: Type (64 bits?)
+			int post_size; 
+			//! number of latest chunk
+			int call_num;
+			//! basis of method data
+			Json::Value json_root;
+			signed int json_parse();
+			void unescape_post(string&);
 	};
 
 	class Rpc_server{
@@ -47,12 +76,12 @@ namespace Annodere{
 			string generate_error(signed int code);
 			struct MHD_Daemon* mhd_daemon;
 			int port;
+		public:
 			static const signed int err_parse=-32700;
 			static const signed int err_method=-32601;
 			static const signed int err_request=-32600;
 			static const signed int err_params=-32602;
 			static const signed int err_internal=-32603;
-		public:
 			Rpc_server(int port=10080);
 			~Rpc_server();
 	};
