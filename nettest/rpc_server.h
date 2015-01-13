e1c2302b2ca9d3b9090cffb2543c2c3cc1962b5e
Thorsten M <thoto@uller.thoto.tk>
2015-01-13 23:25:43 +0100

    Minor changes, const correctness

--- nettest/rpc_server.h
+++ nettest/rpc_server.h
@@ -22,13 +22,13 @@ namespace Annodere{
 
 	class Rpc_method{
 		public:
-			virtual string get_name()=0;
-			virtual vector<Json::ValueType> get_arguments()=0;
-			virtual string call(Json::Value&,Json::Value&){return "";};
+			virtual string get_name() const =0;
+			virtual vector<Json::ValueType> get_arguments() const=0;
+			virtual string call(const Json::Value&,const Json::Value&)=0;
 			virtual ~Rpc_method(){};
-			bool compatible(Json::Value& params);
+			bool compatible(const Json::Value& params) const;
 		protected:
-			string generate_result(Json::Value&, Json::Value&);
+			string generate_result(const Json::Value&, const Json::Value&);
 	};
 
 	class Rpc_method_notify: public Rpc_method{
@@ -36,24 +36,24 @@ namespace Annodere{
 			function<void (const string)> callback;
 		public:
 			Rpc_method_notify(function<void (const string)> cb);
-			string get_name(){return "notify";};
-			vector<Json::ValueType> get_arguments(){
+			string get_name() const {return "notify";};
+			vector<Json::ValueType> get_arguments() const{
 				vector<Json::ValueType> a={Json::stringValue,Json::stringValue};
 				return a;
 			};
 
-			string call(Json::Value&, Json::Value&);
+			string call(const Json::Value&, const Json::Value&);
 	};
 
 	class Rpc_method_register: public Rpc_method{
 		public:
-			string get_name(){return "register";};
-			vector<Json::ValueType> get_arguments(){
+			string get_name() const {return "register";};
+			vector<Json::ValueType> get_arguments() const{
 				vector<Json::ValueType> a={Json::stringValue};
 				return a;
 			};
 
-			string call(Json::Value&, Json::Value&);
+			string call(const Json::Value&, const Json::Value&);
 	};
 	class Rpc_method_wait: public Rpc_method{
 		private:
@@ -63,30 +63,29 @@ namespace Annodere{
 		public:
 			Rpc_method_wait();
 			~Rpc_method_wait();
-			string get_name(){return "wait";};
-			vector<Json::ValueType> get_arguments(){
+			string get_name() const {return "wait";};
+			vector<Json::ValueType> get_arguments() const{
 				vector<Json::ValueType> a={Json::stringValue};
 				return a;
 			};
 
-			string call(Json::Value&, Json::Value&);
-			void send_reply(string);
+			string call(const Json::Value&, const Json::Value&);
+			void send_reply(const string);
 	};
 
 
 	class Rpc_call{
 		public:
-			Rpc_call();
-			Rpc_call(Json::Value);
-			virtual ~Rpc_call(){
-				 if(params!=nullptr) delete params; }
-
 			string method;
 			Json::Value* params;
 			bool id_is_null;
 			Json::Value jv_id;
 
-			int process(const char*,size_t);
+			Rpc_call();
+			Rpc_call(const Json::Value);
+			virtual ~Rpc_call(){
+				 if(params!=nullptr) delete params; }
+			int process(const char*,const size_t);
 			signed int parse();
 			string get_json_rpc();
 		private:
@@ -100,31 +99,33 @@ namespace Annodere{
 			int call_num;
 			//! basis of method data
 			Json::Value json_root;
+
 			signed int json_parse();
-			void unescape_post(string&);
+			void unescape_post(string&) const;
 	};
 
 	class Rpc_server{
 		private:
-			Rpc_call get_method(Json::Value &call);
-			int handler(struct MHD_Connection* connection,
-				const char* cc_url, const char* method, const char* version,
-				const char* upload_data, size_t* upload_data_size,
-				void ** con_cls);
 			struct MHD_Daemon* mhd_daemon;
 			int port;
 			//unordered_multimap<string,Rpc_method*> methods;
 			unordered_map<string,Rpc_method*> methods;
-			string dispatch(string,Json::Value,Json::Value);
+
+			int handler(struct MHD_Connection* connection,
+				const char* cc_url, const char* method, const char* version,
+				const char* upload_data, size_t* upload_data_size,
+				void ** con_cls) const;
+			string dispatch(const string,const Json::Value,const Json::Value)
+				const;
 		public:
 			void register_method(Rpc_method*);
-			static string generate_error(signed int code);
+			static string generate_error(const signed int code);
 			static const signed int err_parse=-32700;
 			static const signed int err_method=-32601;
 			static const signed int err_request=-32600;
 			static const signed int err_params=-32602;
 			static const signed int err_internal=-32603;
-			Rpc_server(int port=10080);
+			Rpc_server(const int port=10080);
 			~Rpc_server();
 	};
 
