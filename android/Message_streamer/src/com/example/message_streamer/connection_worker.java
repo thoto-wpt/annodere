5f5b0bed88588bfe7f8ba80a25f6753eb9c6947d
Thorsten M <thoto@uller.thoto.tk>
2015-01-20 01:49:15 +0100

    Retry and reliable sending of messages
    Also added some comments

--- android/Message_streamer/src/com/example/message_streamer/connection_worker.java
+++ android/Message_streamer/src/com/example/message_streamer/connection_worker.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -22,20 +26,54 @@ import android.util.JsonReader;
 import android.util.JsonToken;
 import android.util.Log;
 
+/**
+ * Worker to handle every kind of network RPC stuff
+ * @author thoto
+ *
+ */
 public class connection_worker {
 	private String session_token;
 	private ConnectivityManager conn_man;
 	private String url;
+	Queue<String> not_queue;
+	Timer retry_timer;
+	String token;
+
 	/**
 	 * Creates connection_worker and connects to server
 	 * @param ip IP address to connect to
 	 * @param token Authentication token to provide
 	 */
 	public connection_worker(Context c,String ip, String token){
-		url="http://"+ip+":10080/annodere";
+		not_queue=new LinkedList<String>();
 		conn_man=(ConnectivityManager)
 				c.getSystemService(Context.CONNECTIVITY_SERVICE);
-		NetworkInfo net_info=conn_man.getActiveNetworkInfo();
+
+		url="http://"+ip+":10080/annodere";
+		this.token=token;
+
+		register();
+
+		retry_timer=new Timer();
+		retry_timer.scheduleAtFixedRate(new retry_timer_task(),500,2000);
+	}
+
+	/**
+	 * enqueue notification to be sent over network
+	 * @param not_str notification text to send
+	 */
+	public void send_notification(String not_str){
+		if(not_queue.isEmpty()){
+			not_queue.add(not_str);
+			process_notification_queue();
+		}else not_queue.add(not_str);
+	}
+
+	/**
+	 * Do RPC-call to register at Desktop application
+	 */
+	private void register(){
+		NetworkInfo net_info=conn_man.getActiveNetworkInfo(); // FIXME REMOVE
 		if (net_info!=null && net_info.isConnected()){
 			JSONArray params=new JSONArray();
 			params.put(token);
@@ -47,27 +85,97 @@ public class connection_worker {
 			System.out.println("No connection available.");
 		}
 	}
-	public void send_notification(String not_str){
-		NetworkInfo net_info=conn_man.getActiveNetworkInfo();
-		if (net_info!=null && net_info.isConnected()){
-			JSONArray params=new JSONArray();
-			params.put(session_token);params.put(not_str);
-			json_request request=new json_request(url,"notify",params);
-			System.out.println("sending ... ");
-			new http_task().execute(request);
-			System.out.println("done");
-		} else {
-			System.out.println("Connection broke.");
+
+	private boolean notification_queue_mutex_up=false;
+	private synchronized boolean notification_queue_mutex(boolean p){
+		if(p){ // set mutex up
+			if(!notification_queue_mutex_up){ // mutex is available
+				notification_queue_mutex_up=true;
+				return true;
+			}else // mutex is unavailable
+				return false;
+		}else{ // set mutex down
+			notification_queue_mutex_up=false;
+			return true;
 		}
 	}
-	protected void set_session_token(String tok){
-		session_token=tok;
+
+	/**
+	 * Do RPC-Call to send oldest message to desktop
+	 */
+	private void process_notification_queue(){
+		if(!notification_queue_mutex(true)) return; // processing in progress
+		String not_str=not_queue.peek();
+		if(not_str==null) return; // no more to send
+
+		NetworkInfo net_info=conn_man.getActiveNetworkInfo(); // FIXME Remove
+		if (net_info!=null && net_info.isConnected()){
+			// compose request
+			JSONArray params=new JSONArray();
+			if(session_token==null){
+				Log.d("CW","session token null");
+				register();
+				return;
+			}
+			params.put(session_token);params.put(not_str);
+			Log.d("CW","Connection Token:"+session_token);
+			json_request request=new json_request(url,"notify",params);
+			// send
+			Log.d("CW","sending ... ");
+			new http_task().execute(request);
+		} else {
+			Log.e("CW","Connection broke.");
+		}
 	}
+
+	/**
+	 * Callback method to call after call returned and reception is complete
+	 * @param res response
+	 */
 	protected void callback(json_result res){
-		// TODO -> should be removed and arbitrary method included in request
-		if(res.rq.method.equals("register")) set_session_token(res.str_val);
+		// process result of register call: Set session token
+		if(res.rq.method.equals("register")) session_token=res.str_val;
+		else if(res.rq.method.equals("notify")){
+			notification_queue_mutex(false); // make method available again
+			Log.d("CW","Notification Result: ");
+			if(res.val_type==3 && res.bool_val==true){
+				Log.d("CW","OK!");
+				not_queue.poll(); // successfully sent: remove top element
+				if(!not_queue.isEmpty()) process_notification_queue();
+			}else{
+				try { //wait some time to prevent flooding
+					Thread.sleep(500);
+				} catch (InterruptedException e) {} // don't care
+				if(res.val_type==2){
+					Log.d("CW","Unregistered");
+					// unregistered. Try to re-register
+					register();
+				}else{
+					Log.d("CW","err "+res.val_type);
+					// could not send: retry!
+					process_notification_queue(); // retry
+				}
+			}
+		}
 	}
-	
+
+	/**
+	 * Timer to periodically retry sending notifications
+	 * @author thoto
+	 *
+	 */
+	private class retry_timer_task extends TimerTask{
+		@Override
+		public void run() {
+			if(!not_queue.isEmpty()) process_notification_queue();
+		}
+	}
+
+	/**
+	 * JSON RPC call request structure
+	 * @author thoto
+	 *
+	 */
 	private class json_request{
 		public String method;
 		public JSONArray params;
@@ -95,18 +203,29 @@ public class connection_worker {
 			}
 		}
 	}
+
+	/**
+	 * result structure emitted by JSON RPC calls
+	 * @author thoto
+	 *
+	 */
 	private class json_result{
-		public int val_type=0;
+		public int val_type=0; //! 0: Err 1: Str, 2: Num, 3: Bool, 4: Null
 		public boolean err=true;
 		public String str_val;
 		public Integer int_val;
 		public boolean bool_val;
 		public boolean err_critical;
 		public json_request rq;
-		
+
+		/**
+		 * return result
+		 * @param reader read value from this reader
+		 * @param rq request to include reference to
+		 */
 		public json_result(JsonReader reader,json_request rq){
 			JsonToken json_type;
-			
+
 			this.rq=rq;
 			try {
 				json_type = reader.peek();
@@ -131,19 +250,30 @@ public class connection_worker {
 				e.printStackTrace();
 			}
 		}
+
+		/**
+		 * Constructor for error result
+		 * @param b is error critical?
+		 * @param jr reader to skip element
+		 * @param rq request to include reference to
+		 * @throws IOException
+		 */
 		public json_result(boolean b,JsonReader jr,json_request rq) 
 				throws IOException{//error case
 			this.rq=rq;
 			jr.skipValue();
-			
+
 			err=true;err_critical=b;
 			str_val="some error TODO"; // TODO
 		}
 
+		/**
+		 * make printable string for debugging purposes
+		 */
 		@Override
 		public String toString(){
 			if(err) return "EE"+(err_critical?"C":"E")+": "+str_val;
-			
+
 			switch(val_type){
 			case 1:  return str_val;
 			case 2:  return int_val.toString();
@@ -153,9 +283,16 @@ public class connection_worker {
 			}
 		}
 	}
+
+	/**
+	 * Task to execute HTTP request
+	 * @author thoto
+	 *
+	 */
 	private class http_task extends AsyncTask<json_request,Integer,json_result>{
 		@Override
 		protected json_result doInBackground(json_request... req) {
+			// initialization
 			HttpClient client;
 			HttpPost post;
 			HttpResponse response;
@@ -166,6 +303,7 @@ public class connection_worker {
 			String jn;
 			
 			try {
+				// Thread.sleep(6000); TODO TEST POINT!
 				// send HTTP request
 				client=new DefaultHttpClient();
 				post=new HttpPost(req[0].get_url());
@@ -174,26 +312,27 @@ public class connection_worker {
 				
 				// parse response
 				inputStream = response.getEntity().getContent();
-				if(inputStream != null){ // parse JSON part
+				if(inputStream != null){
 					jr=new JsonReader(new BufferedReader(
 							new InputStreamReader(inputStream)));
 					
+					// parse JSON part
 					jr.beginObject();
 					while(jr.hasNext()){
 						jn=jr.nextName();
 						if(jn.equals("result")){ // return result
 							res=new json_result(jr,req[0]);
-						}else if(jn.equals("jsonrpc")){
+						}else if(jn.equals("jsonrpc")){ // check version
 							String version=jr.nextString();
 							if(!version.equals("2.0")){
 								jr.close();
 								return null;
 							}
-						}else if(jn.equals("error")){
+						}else if(jn.equals("error")){ // return error TODO
 							res=new json_result(false,jr,req[0]);
-						}else if(jn.equals("id")){
+						}else if(jn.equals("id")){ // don't handle id TODO
 							jr.skipValue();
-						}else{
+						}else{ // unknown thingy
 							Log.d("JSON","skip.");
 							jr.skipValue();
 						}
@@ -201,7 +340,6 @@ public class connection_worker {
 					jr.close();
 					inputStream.close();
 					if(res!=null) System.out.println("Result: "+res.toString());
-					Thread.sleep(6000);
 					callback(res);
 					return res;
 				}
